331046649de3c426a6d320aa5b2a22d55689c09e
Tingan Zhu <89436540+Tinganz@users.noreply.github.com>
2023-10-13 11:59:14 -0400

    testing message

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
         serialPort.start();
 
         mavlink_message_t message;
-        
+        message = create_heartbeat_message();
 
         // Send the message
         int bytesWritten = serialPort.write_message(message);
@@ -27,4 +27,22 @@ int main() {
     serialPort.stop();
 
     return 0;
+}
+
+mavlink_message_t create_heartbeat_message() {
+    mavlink_message_t message;
+    mavlink_heartbeat_t heartbeat;
+    memset(&heartbeat, 0, sizeof(heartbeat));
+
+    // Set the values for the heartbeat message
+    heartbeat.type = MAV_TYPE_GCS; // Type of the system sending the message
+    heartbeat.autopilot = MAV_AUTOPILOT_GENERIC; // Autopilot type
+    heartbeat.base_mode = MAV_MODE_GUIDED_ARMED; // System mode
+    heartbeat.custom_mode = 0; // Custom mode (if applicable)
+    heartbeat.system_status = MAV_STATE_ACTIVE; // System status
+
+    // Pack the heartbeat message into the MAVLink message
+    mavlink_msg_heartbeat_encode(1, 200, &message, &heartbeat);
+
+    return message;
 }
